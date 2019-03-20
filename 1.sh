ea66ff888e71bd06bba04ea6e72bbffaa47aa107
jishnu <jishnuekd4@gmail.com>
2019-03-20 21:08:50 +0530

    1st qn

---
+++ 1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+echo -e "current user :$USER (user name: $LOGNAME)"
+echo -e "current shell :$SHELL"
+echo -e "home :$HOME"
+echo -e "OS :$OSTYPE"
+echo -e "path :$PATH"
+echo -e "current working directory :$PWD"
+echo -e "no.of users logged in :`who | wc -l`"
