a9775ce5ecccb51da13791d388e8f00d1e032666
sachin-kizhakkayil-1998 <saching1998@cet.ac.in>
2019-03-28 01:02:50 +0530

    modified

--- 5.sh
+++ 5.sh
@@ -1,5 +1,7 @@
 max_mem=10.0
 current_pgm=`pwd`/$0
+while true
+do
 echo "sh $current_pgm" >> ~/.bashrc
-MEM=$1
 kill `ps -o pid,%mem ax | sort -b -k2 -r | awk -F " " '$2>$max_mem' | awk -F "  " '{print $1}'`
+done
